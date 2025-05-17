a5cb31734d1f53f48793fc30fbd6eb44078a5cd1
Hayden <haydenk@users.noreply.github.com>
2025-05-17 17:12:12 -0500

    move to zsh custom directory

---
+++ .config/zsh/plugins/umask/umask.plugin.zsh
@@ -0,0 +1,17 @@
+# set umask so that only the user has access to files/directories created
+umask u=rwx,go=
+
+function _umask_hook {
+    if [[ $PWD == $HOME/Projects/* || $PWD == $HOME/Projects ]]; then
+        umask u+rwx,g=rx,o=r
+    else
+        umask u+rwx,go=
+    fi
+}
+
+# zsh hooks
+# trigger _umask_hook once working dir is changed
+# precmd is not enough, cause it may not be triggered when cwd is changed by ZLE widget
+add-zsh-hook chpwd _umask_hook
+# make sure _umask_hook is run on startup
+add-zsh-hook precmd _umask_hook
