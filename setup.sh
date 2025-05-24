03dfd7b9b7d82b99c771b3da5975c98d247e1a16
Hayden <haydenk@users.noreply.github.com>
2025-05-24 03:20:37 +0000

    reconfigure setup script

--- setup.sh
+++ setup.sh
@@ -5,12 +5,6 @@ set -e
 ZSH_AUTOSUGGESTIONS_VERSION="v0.7.1"
 ZSH_SYNTAX_HIGHLIGHTING_VERSION="0.8.0"
 
-mkdir -vp ~/.config/zsh/plugins/zsh-autosuggestions && curl -sSL
-https://github.com/zsh-users/zsh-autosuggestions/archive/refs/tags/$ZSH_AUTOSUGGESTIONS_VERSION.tar.gz | tar xvz --strip-components=1 -C ~/.config/zsh/plugins/zsh-autosuggestions/
-
-mkdir -vp ~/.config/zsh/plugins/zsh-syntax-highlighting && curl -sSL
-https://github.com/zsh-users/zsh-syntax-highlighting/archive/refs/tags/$ZSH_SYNTAX_HIGHLIGHTING_VERSION.tar.gz | tar xvz --strip-components=1 -C ~/.config/zsh/plugins/zsh-syntax-highlighting/
-
 sudo mkdir -p /etc/apt/keyrings
 wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
@@ -27,4 +21,10 @@ git clone --bare /workspaces/.codespaces/.persistedshare/dotfiles $HOME/.cfg
 cd
 config checkout master
 config config --local status.showUntrackedFiles no
+
+mkdir -vp ~/.config/zsh/plugins/zsh-autosuggestions
+curl -sSL https://github.com/zsh-users/zsh-autosuggestions/archive/refs/tags/$ZSH_AUTOSUGGESTIONS_VERSION.tar.gz | tar xvz --strip-components=1 -C ~/.config/zsh/plugins/zsh-autosuggestions/
+
+mkdir -vp ~/.config/zsh/plugins/zsh-syntax-highlighting
+curl -sSL https://github.com/zsh-users/zsh-syntax-highlighting/archive/refs/tags/$ZSH_SYNTAX_HIGHLIGHTING_VERSION.tar.gz | tar xvz --strip-components=1 -C ~/.config/zsh/plugins/zsh-syntax-highlighting/
 
