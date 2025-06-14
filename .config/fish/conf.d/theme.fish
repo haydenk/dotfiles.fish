bfc0b97560836b86e04f1d2a35c82af1210bc46a
Hayden <haydenk@users.noreply.github.com>
2025-06-14 03:31:40 +0000

    add some fish and git ignore changes from macos branch

--- .config/fish/conf.d/theme.fish
+++ .config/fish/conf.d/theme.fish
@@ -4,31 +4,43 @@ set -x -g JQ_COLORS "0;34:0;37:0;37:0;37:0;33:1;37:1;37:0;32"
 
 set -x -g TERM xterm-256color
 
-# Prompt mods
-set fish_color_autosuggestion 3f4964
-set fish_color_cancel -r
-set fish_color_command --bold
-set fish_color_comment red
-set fish_color_cwd green
-set fish_color_cwd_root red
-set fish_color_end brmagenta
-set fish_color_error brred
-set fish_color_escape bryellow --bold
-set fish_color_history_current --bold
-set fish_color_host normal
-set fish_color_host_remote yellow
-set fish_color_match --background=brblue
-set fish_color_normal normal
-set fish_color_operator bryellow
-set fish_color_param cyan
-set fish_color_quote yellow
-set fish_color_redirection brblue
-set fish_color_search_match bryellow --background=brblack
-set fish_color_selection white --bold --background=brblack
-set fish_color_status red
-set fish_color_user brgreen
-set fish_color_valid_path --underline
-set fish_pager_color_completion
-set fish_pager_color_description B3A06D yellow
-set fish_pager_color_prefix white --bold --underline
-set fish_pager_color_progress brwhite --background=blue
+# Colorscheme: ayu Dark
+set -U fish_color_normal B3B1AD
+set -U fish_color_command 39BAE6
+set -U fish_color_keyword 39BAE6
+set -U fish_color_quote C2D94C
+set -U fish_color_redirection FFEE99
+set -U fish_color_end F29668
+set -U fish_color_error FF3333
+set -U fish_color_param B3B1AD
+set -U fish_color_comment 626A73
+set -U fish_color_match F07178
+set -U fish_color_selection --background=E6B450
+set -U fish_color_search_match --background=E6B450
+set -U fish_color_history_current --bold
+set -U fish_color_operator E6B450
+set -U fish_color_escape 95E6CB
+set -U fish_color_cwd 59C2FF
+set -U fish_color_cwd_root red
+set -U fish_color_option B3B1AD
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion 4D5566
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_host_remote yellow
+set -U fish_color_history_current --bold
+set -U fish_color_status red
+set -U fish_color_cancel --reverse
+set -U fish_pager_color_prefix normal --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D
+set -U fish_pager_color_selected_background --background=E6B450
+set -U fish_pager_color_secondary_background 
+set -U fish_pager_color_secondary_completion 
+set -U fish_pager_color_secondary_prefix 
+set -U fish_pager_color_selected_completion 
+set -U fish_pager_color_background 
+set -U fish_pager_color_selected_prefix 
+set -U fish_pager_color_selected_description 
+set -U fish_pager_color_secondary_description 
